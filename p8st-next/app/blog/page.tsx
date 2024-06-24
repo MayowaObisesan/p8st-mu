c06e7f5537c4bb81787a044de23ddc3b4f424c6c
MayowaObisesan <mayowaobi74@gmail.com>
2024-06-24 17:08:32 +0100

    Update build file

--- p8st-next/app/blog/page.tsx
+++ p8st-next/app/blog/page.tsx
@@ -3,8 +3,7 @@ import { title } from "@/components/primitives";
 export default function BlogPage() {
   return (
     <div>
-      <h1 className={title()}>Arcade Page</h1>
-      <h2>Coming Soon...</h2>
+      <h1 className={title()}>Blog</h1>
     </div>
   );
 }
